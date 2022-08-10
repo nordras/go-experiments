b3716f8808e5501f13b803d4ede343c96bdf5e18
Igor <igorkmail@gmail.com>
2022-08-10 11:47:04 -0300

    variables

--- main.go
+++ main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main()  {
 	fmt.Print("Hello world \n")
+
+	// variables
+	const userName string = "Igor";
+	var age int = 30
+	occupation := "software engineer"
+
+	fmt.Println("Igor", age)
+	fmt.Printf("%v, %t", occupation, occupation)
 }
